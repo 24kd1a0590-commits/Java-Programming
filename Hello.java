e8d23400e01ba54190e5931da817bf1e1d52e7d0
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-06 18:42:10 +0530

    To use : instead of -> in switch

--- Hello.java
+++ Hello.java
@@ -7,9 +7,9 @@ public static void main(String args[])
    String result="";
    result=switch(day)
    {
-     case "Saturday","Sunday" -> "6am";
-      case "Monday"->"8am";
-      default ->"7am";
+     case "Saturday","Sunday" :yield "6am";
+      case "Monday": yield "8am";
+      default :yield "7am";
     
    };
    System.out.println(result);
