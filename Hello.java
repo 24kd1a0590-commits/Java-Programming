05d637bbd0115f811f40b63f9bad7e78816b94da
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-06 19:22:46 +0530

    Do while loop

--- Hello.java
+++ Hello.java
@@ -2,17 +2,11 @@ class Hello
 {
 public static void main(String args[])
 {
-   int i=1;
-   while(i<=4)
+   int i=5;
+   do
    {
       System.out.println("Hi " + i );
-      int j=1;
-      while(j<=3)
-      {
-         System.out.println("Hello "+ j);
-         j++;
-      }
        i++;
-   }
+   }while(i<=4);
 }
 }
