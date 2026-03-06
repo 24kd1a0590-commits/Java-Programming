25ea67debe8dd2e410283e4fc66a9dbc715f629c
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-06 19:17:56 +0530

    While and inner while

--- Hello.java
+++ Hello.java
@@ -2,16 +2,17 @@ class Hello
 {
 public static void main(String args[])
 {
-   int num=10;
-   String day="Sunday";
-   String result="";
-   result=switch(day)
+   int i=1;
+   while(i<=4)
    {
-     case "Saturday","Sunday" :yield "6am";
-      case "Monday": yield "8am";
-      default :yield "7am";
-    
-   };
-   System.out.println(result);
+      System.out.println("Hi " + i );
+      int j=1;
+      while(j<=3)
+      {
+         System.out.println("Hello "+ j);
+         j++;
+      }
+       i++;
+   }
 }
 }
