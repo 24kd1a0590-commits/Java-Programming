64676069af294ab51b7dcc808c14e3489f9a33c7
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-12 19:08:18 +0530

    Array of objects

--- demo.java
+++ demo.java
@@ -1,27 +1,33 @@
+class Student
+{
+   int rollno;
+   String name;
+   double marks;
+}
 class demo
 {
    public static void main(String args[])
    {
-      int nums[][]=new int[3][];
-      nums[0]=new int[3];
-      nums[1]=new int[2];
-      nums[2]=new int[3];
-      for(int i=0;i<nums.length;i++)
-      {
-         for(int j=0;j<nums[i].length;j++)
-         {
-            nums[i][j]=(int)(Math.random()*10);
-         }
-      }
-   
-      for(int n[]: nums)
-      {
-         for(int m :n)
-         {
-            System.out.print(m+" ");
-         }
-         System.out.println();
+      Student s1=new Student();
+      s1.rollno=1;
+      s1.name="Prathyusha";
+      s1.marks=99;
+      Student s2=new Student();
+      s2.name="Tulasi";
+      s2.rollno=2;
+      s2.marks=88;
+      Student s3=new Student();
+      s3.rollno=3;
+      s3.name="Vaish";
+      s3.marks=89;
 
+      Student students[]=new Student[3];
+      students[0]=s1;
+      students[1]=s2;
+      students[2]=s3;
+      for(int i=0;i<students.length; i++)
+      {
+         System.out.println(students[i].name +" : "+ students[i].marks);
       }
    }
 }
