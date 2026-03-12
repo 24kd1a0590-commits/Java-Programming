e74f17f0124690400efe22801d33a39283f402b6
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-12 18:11:30 +0530

    Static Creation of Array

--- demo.java
+++ demo.java
@@ -1,22 +1,8 @@
-class calculator
-{
-   public int add(int n1,int n2,int n3)
-   {
-      return n1+n2+n3;
-   }
-   public int add(int n1,int n2)
-   {
-      return n1+n2;
-   }
-}
 class demo
 {
    public static void main(String args[])
    {
-      calculator obj=new calculator();
-      int r1=obj.add(3,4,5);
-      int r2=obj.add(7,6);
-      System.out.println(r1);
-      System.out.println(r2);
+      int nums[]={3,4,5,6};
+      System.out.println(nums[0]);
    }
 }
