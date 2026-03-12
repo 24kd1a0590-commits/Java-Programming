6f1fe275f74c6f6760f1b61fd3e7a67ad696c361
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-12 18:48:33 +0530

    Jagged Array

--- demo.java
+++ demo.java
@@ -2,10 +2,13 @@ class demo
 {
    public static void main(String args[])
    {
-      int nums[][]=new int[3][4];
-      for(int i=0;i<3;i++)
+      int nums[][]=new int[3][];
+      nums[0]=new int[3];
+      nums[1]=new int[2];
+      nums[2]=new int[3];
+      for(int i=0;i<nums.length;i++)
       {
-         for(int j=0;j<4;j++)
+         for(int j=0;j<nums[i].length;j++)
          {
             nums[i][j]=(int)(Math.random()*10);
          }
