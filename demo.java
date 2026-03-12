2fb62021f20baaef181f095c453f8ec993a6dba0
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-12 19:14:25 +0530

    Array of objects printing using for each loop

--- demo.java
+++ demo.java
@@ -25,9 +25,9 @@ class demo
       students[0]=s1;
       students[1]=s2;
       students[2]=s3;
-      for(int i=0;i<students.length; i++)
-      {
-         System.out.println(students[i].name +" : "+ students[i].marks);
-      }
+     for(Student stud: students)
+     {
+      System.out.println(stud.name + ":" + stud.marks);
+     }
    }
 }
