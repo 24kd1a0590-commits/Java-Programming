7afed6c142e6aea30983b5761f3d57fd309223db
24kd1a0590-commits <24kd1a0590@lendi.edu.in>
2026-03-12 18:22:26 +0530

    Dynamic Array creation

--- demo.java
+++ demo.java
@@ -2,7 +2,15 @@ class demo
 {
    public static void main(String args[])
    {
-      int nums[]={3,4,5,6};
-      System.out.println(nums[0]);
+      int nums[]= new int[5];
+      nums[0]=1;
+      nums[1]=2;
+      nums[2]=3;
+      nums[3]=4;
+      nums[4]=5;
+      for(int i=0;i<5;i++)
+      {
+      System.out.println(nums[i]);
+      }
    }
 }
